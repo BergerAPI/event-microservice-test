2d55ed8dfebca80db339660d2263eeae5396f3f4
BergerAPI <bergerbtw@gmail.com>
2022-07-16 13:36:08 +0200

    Removed unused env-variable

--- src/tester/src/index.ts
+++ src/tester/src/index.ts
@@ -16,7 +16,7 @@ import {initService} from "./service/service";
 
     initService();
 
-    Express.listen(process.env.PORT || 3000, () => {
+    Express.listen(3000, () => {
         console.log("Express is listening.")
     });
 })().then(() => {
